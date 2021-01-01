78e6f9d245603d49084c622efbe1848f7cc435db
Juan Yanac <juan.yanac@outlook.com>
2021-01-01 19:22:10 -0300

    adding Book Model

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key] //primary key
+        public int Id { get; set; }
+        [Required] // not null
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
