227ce8c023744aee072a285e330137670a8cfb32
Juan Yanac <juan.yanac@outlook.com>
2021-01-01 21:51:12 -0300

    Add Book table to database

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext // herda desde EntityFrameworkCore
+    {
+        ///contrutor para passar os paametros da conexao, 
+        ///este es um construtor vacio mas os parametros sao necesarios para a injecao de depndencia
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
