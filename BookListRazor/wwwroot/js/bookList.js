cea5bd7d1655324fe910684575e040adeec46694
Juan Yanac <juan.yanac@outlook.com>
2021-01-03 02:47:01 -0300

    Delete Book

--- BookListRazor/wwwroot/js/bookList.js
+++ BookListRazor/wwwroot/js/bookList.js
@@ -2,14 +2,14 @@
 
 $(document).ready(function () {
     loadDataTable();
-})
+});
 
 function loadDataTable() {
     dataTable = $('#DT_load').dataTable({
         "ajax": {
             "url": "api/Book",
             "type": "GET",
-            "dataTtype":"json"
+            "datatype": "json"
         },
         "columns": [
             { "data": "name", "width": "20%" },
@@ -20,14 +20,43 @@ function loadDataTable() {
                 "render": function (data) {
                     return `<div class="text-center">
                                 <a href="/BookList/Edit?id=${data}" class="btn btn-success" style='cursor:pointer; width:100px;'>Edit</a>
-                                <a class="btn btn-danger text-white" style='cursor:pointer; width:100px;'>Delete</a>
+                                <a onclick=Delete('/api/Book?id='+${data}) class="btn btn-danger text-white" style='cursor:pointer; width:100px;'>Delete</a>
                             </div>`;
-                }, "width":"40%"
+                }, "width": "40%"
             }
         ],
         "language": {
-            "emptyTable":"no data found"
+            "emptyTable": "no data found"
         },
-        "width":"100%"
+        "width": "100%"
+    });
+}
+
+function Delete(url) {
+    swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        //dataTable.ajax.reload();
+                        location.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
     });
 }
